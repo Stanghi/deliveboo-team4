15f783275f4a45f4c816a03dd449b1b4b5d8b4e4
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-15 15:57:08 +0100

    add migration and seeder for products table

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public static function generateSlug($string)
+    {
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+        $c = 1;
+        $exist = Product::where('slug', $slug)->first();
+
+        while ($exist) {
+            $slug = $original_slug . '-' . $c;
+            $exist = Product::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
+}
---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = config('products.products');
+        foreach($products as $product) {
+            $new_product = new Product();
+            $new_product->restaurant_id = $product['restaurant_id'];
+            $new_product->name = $product['name'];
+            $new_product->slug = Product::generateSlug($new_product->name);
+            $new_product->is_visible = $product['isVisible'];
+            $new_product->description = $product['description'];
+            $new_product->price = $product['price'];
+            $new_product->img = $product['img'];
+            $new_product->img_original_name = str_replace('uploads/', '', $product['img']);
+
+            $new_product->save();
+        }
+    }
+}

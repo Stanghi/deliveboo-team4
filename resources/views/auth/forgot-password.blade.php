12018c173b793b38c094db67ce24fab8dafde1c7
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-19 22:49:00 +0100

    add some style minor changes

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -36,7 +36,7 @@
 
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-dark">
                                     {{ __('Invia il link per il reset della password') }}
                                 </button>
                             </div>

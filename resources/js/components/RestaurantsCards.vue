529ab881f27874cc26fd7cea350095967b70f86c
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-20 14:26:09 +0100

    fix import components in router

---
+++ resources/js/components/RestaurantsCards.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "RestaurantsCards",
+};
+</script>
+
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<style>
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,11 +1,17 @@
 <script>
+import RestaurantsCards from '../components/RestaurantsCards.vue';
 export default {
     name: "Home",
+    components: {
+        RestaurantsCards
+    }
 };
 </script>
 
 <template>
     <h1>Home</h1>
+    <RestaurantsCards />
+
 </template>
 
 <style></style>

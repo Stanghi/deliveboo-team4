4a1a24929a0733822db068c9bb4a377686fcd422
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-20 14:57:15 +0100

    add basic cards where to print restaurants

---
+++ resources/js/components/RestaurantItem.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "RestaurantItem",
+};
+</script>
+
+<template>
+    <div class="col">
+        <div class="card d-flex flex-row mb-4">
+            <img src="../../img/placeholder.png" alt="placeholder" />
+            <div class="card-body">
+                <h5 class="card-title">Ristorante La Medusa</h5>
+                <p class="card-text address">
+                    Via Roma, 25 Milano
+                </p>
+                <span class="category">Categoria</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+.card {
+    border-radius: 10px;
+}
+img {
+    height: 50px;
+}
+</style>
--- resources/js/components/RestaurantsCards.vue
+++ resources/js/components/RestaurantsCards.vue
@@ -1,12 +1,18 @@
 <script>
+import RestaurantItem from './RestaurantItem.vue';
 export default {
     name: "RestaurantsCards",
+    components: {
+        RestaurantItem
+    }
 };
 </script>
 
 <template>
     <div class="container">
-
+        <div class="row row-cols-3">
+            <RestaurantItem v-for="i in 10" :key="i" />
+        </div>
     </div>
 </template>
 

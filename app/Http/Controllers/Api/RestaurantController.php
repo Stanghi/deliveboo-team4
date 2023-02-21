6357be9d9a8c3d252f1f47b23c0ac60791491a76
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-21 20:08:12 +0100

    erase comments

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -18,11 +18,6 @@ class RestaurantController extends Controller
     }
 
     public function getByCategory($values) {
-        // $stringValues = str_replace(',','', $values);
-        // $categoryChoice = str_split($stringValues);
-        // if(str_contains($values, ',')) {
-
-        // }
         $categoryChoice = explode(',',$values );
         $restaurants = Restaurant::with('categories')->whereHas('categories', function(Builder $query) use($categoryChoice) {
             $query->whereIn('category_id', $categoryChoice)
@@ -31,13 +26,5 @@ class RestaurantController extends Controller
         })->get();
 
         return response()->json(compact('restaurants'));
-
-
-        // $list_restaurants = [];
-        // $categories = Category::where('id', $values)->with(['restaurants'])->first();
-        // foreach ($categories->restaurants as  $restaurant) {
-        //     $list_restaurants[] = Restaurant::where('id', $restaurant->id)->with(['categories'])->first();
-        // }
-        // return response()->json($list_restaurants );
     }
 }

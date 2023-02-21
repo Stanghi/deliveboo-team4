4248ad382885eca4bcce608a8029463d10070f66
Stanghi <stangherlin.matti@gmail.com>
2023-02-21 11:00:00 +0100

    start fix category card

--- resources/js/components/Category.vue
+++ resources/js/components/Category.vue
@@ -1,54 +1,53 @@
 <!-- Ciao Federico :) -->
 <script>
-import axios from 'axios';
-import {baseUrl} from '../data/data';
-import {store} from '../data/store';
-import CategoryCard from './CategoryCard.vue';
+import axios from "axios";
+import { baseUrl } from "../data/data";
+import { store } from "../data/store";
+import CategoryCard from "./CategoryCard.vue";
 
 export default {
-  components: {CategoryCard,
-   },
-  name:'Category',
-  data(){
-    return {
-        baseUrl,
-        store
-    }
-  },
-  methods:{
-    getApi() {
-            axios.get(this.baseUrl + 'restaurants').then((result) => {
-                store.restaurants= result.data.restaurants;
-                store.categories= result.data.categories;
+    components: { CategoryCard },
+    name: "Category",
+    data() {
+        return {
+            baseUrl,
+            store,
+        };
+    },
+    methods: {
+        getApi() {
+            axios.get(this.baseUrl + "restaurants").then((result) => {
+                store.restaurants = result.data.restaurants;
+                store.categories = result.data.categories;
                 console.log(result.data);
             });
         },
 
-      getCategories(category_id){
-         axios.get(this.baseUrl + 'restaurants/getCategories/' + category_id )
-         .then(result=>{
-             console.log(result.data);
-         })
-      }
-  },
-  mounted(){
-    this.getApi()
-    this.getCategories()
-  }
-}
+        getCategories(category_id) {
+            axios
+                .get(this.baseUrl + "restaurants/getCategories/" + category_id)
+                .then((result) => {
+                    console.log(result.data);
+                });
+        },
+    },
+    mounted() {
+        this.getApi();
+        this.getCategories();
+    },
+};
 </script>
 
 <template>
-  <div class="container debug">
-    <h1>Categorie</h1>
-    <div class="d-flex justify-content-between debug">
+    <div
+        class="container p-0 d-flex justify-content-between align-items-center flex-wrap"
+    >
         <CategoryCard
-        v-for="category in store.categories" :key="category.id" :category="category"
+            v-for="category in store.categories"
+            :key="category.id"
+            :category="category"
         />
     </div>
-  </div>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>

3e83e6bdde80bc27c192a9e6f015994da7b2bf11
manuelmenama <manuel.mena090589@gmail.com>
2023-02-21 10:52:37 +0100

    added store in search

--- resources/js/components/SearchBar.vue
+++ resources/js/components/SearchBar.vue
@@ -1,10 +1,13 @@
-<!-- Ciao Manuel :) -->
 <script>
+
+import { store } from '../data/store';
+
 export default {
     name: "SearchBar",
     data(){
         return{
-            searched: ''
+            searched: '',
+            store
         }
     },
     methods:{
@@ -19,22 +22,39 @@ export default {
     <div class="container">
         <div class="row">
 
-            <div class="col-8 offset-2 debug d-flex align-items-center">
-                <input
-                    type="text" class="form-control me-1" id="search"
+            <div class="col-8 offset-2 d-flex align-items-center">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control my-input" aria-label="Recipient's username" aria-describedby="button-addon2"
                     v-model="searched"
                     @keyup.enter="startSearch()"
                     placeholder="Cerca il tuo ristorante...">
-                <button type="button" class="btn btn-primary" @click="startSearch()">Cerca</button>
+                    <button class="btn my-btn-color" type="button" id="button-addon2" @click="startSearch()">Cerca</button>
+                </div>
+
+
 
             </div>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
+@use "../../scss/_variables.scss" as *;
 
 .debug{
     background-color: rgba(255, 0, 0, 0.137);
+}
+.my-input{
+    box-shadow: inset 1px 1px 1px rgba(0, 0, 0, 0.5);
+    border: 0;
+}
+.my-btn-color{
+    background-color: $orange;
+    color: $white;
+    &:hover{
+        background-color: white;
+        border: 1px solid $orange;
+        color: $orange;
+    }
 }
 
 </style>

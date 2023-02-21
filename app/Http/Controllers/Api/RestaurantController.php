565f9bcbae67c7b18588433b7199f5f98c2237cc
Stanghi <stangherlin.matti@gmail.com>
2023-02-21 20:15:31 +0100

    fix select in RestaurantController

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -10,19 +10,22 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $restaurants = Restaurant::all();
         $categories = Category::all();
 
         return response()->json(compact('restaurants', 'categories'));
     }
 
-    public function getByCategory($values) {
-        $categoryChoice = explode(',',$values );
-        $restaurants = Restaurant::with('categories')->whereHas('categories', function(Builder $query) use($categoryChoice) {
+    public function getByCategory($values)
+    {
+        $categoryChoice = explode(',', $values);
+        $restaurants = Restaurant::with('categories')->whereHas('categories', function (Builder $query) use ($categoryChoice) {
             $query->whereIn('category_id', $categoryChoice)
-              ->groupBy('restaurant_id')
-              ->havingRaw('COUNT(DISTINCT category_id) = '.count($categoryChoice));
+                ->groupBy('restaurant_id')
+                ->select('restaurant_id')
+                ->havingRaw('COUNT(DISTINCT category_id) = ' . count($categoryChoice));
         })->get();
 
         return response()->json(compact('restaurants'));

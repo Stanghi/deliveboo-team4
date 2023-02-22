63f6d8d610c3b25a7020a192109665ae345e961e
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-22 12:21:51 +0100

    add component product detail where increase/decrese quantity in cart

--- resources/js/class/Cart.js
+++ resources/js/class/Cart.js
@@ -1,106 +1,81 @@
-//Importazione classe CartItem
 import CartItem from "./CartItem.js";
 
-//DEFINIZIONE DELLA CLASSE CART
 export default class Cart {
-  //Costruttore della classe Cart
-  constructor(_restaurant = null, _items = []) {
-    this._restaurant = _restaurant;
-    this._items = _items;
-  }
-
-  //Getter o sorta di proprietà custom dell'oggetto Cart che mi restituisce il totale del carrello
-  get amount() {
-    return this._items.reduce((sum, item) => sum + item.totalPrice, 0);
-  }
-
-  //Getter o sorta di proprietà custom della classe Cart che mi restituisce il numero totale di items(oggetti della classe ClassItem che hanno tra le proprietà il prodotto e la quantità)
-  get totalQuantity() {
-    return this._items.reduce((sum, item) => item.quantity, 0);
-  }
-
-   //Getter o sorta di proprietà custom della classe Cart che mi restituisce la proprietà items ovvero l'array di istanze della classe ClassItem dove abbiamo i prodotti del carrello
-  get items() {
-    return this._items;
-  }
-
-  //Getter o sorta di proprietà custom della classe Cart che mi restituisce la proprietà restaurant dove ho salvato il riferimento del ristorante del carrello
-  get restaurant() {
-    return this._restaurant;
-  }
-
-  //Metodo della classe Cart che mi restituisce se il carrello è vuoto (controllando la length della proprietà items che è un array)
-  isEmpty() {
-    return this._items.length === 0;
-  }
-
-  //METODO FINDITEM()
-  //Metodo della classe Cart che chiede come argomenti un prodotto esterno che voglio cercare se sia presente nel carrello e il ristorante del prodotto che sto cercando. Il metodo controlla prima di tutto se c'è corrispondenza tra il ristorante del prodotto e il ristorante del mio carrello. Poi lo cerca e se lo trova restituisce l'item del carrello che corrisponde a quel prodotto.
-  findItem(product) {
-    if (this._restaurant && this._restaurant.id === product.restaurant_id) {
-      return this._items.find((item) => item.product.id === product.id);
-    } else {
-      return null;
+    constructor(_restaurant = null, _items = []) {
+        this._restaurant = _restaurant;
+        this._items = _items;
     }
-  }
 
-  //METODO ADDITEM()
-  //Metodo della classe Cart che mi permette:
-  //1. Se il carrello è vuoto (lo controllo chiamando il metodo isEmpty() di Cart) mi permette di aggiungere un nuovo prodotto al carrello. In questo caso l'aggiunta del nuovo prodotto avviene istanziando un nuovo oggetto della classe CartItem e passo al costruttore
-  //il ristorante e il prodotto che verranno salvati nelle proprietà restaurante e product dell'oggetto. Non passando la quantity il default è 1.
-  //2. Se il carrello non è vuoto, controllo prima che ci sia un matching tra il ristorante del prodotto che voglio aggiungere e il ristorante del carrello (salvato nella proprietà restaurant di Cart). Se c'è corrispondenza, controllo se il prodotto che voglio aggiungere sia già presente nel mio carrello chiamando il metodo di Cart findItem che mi restituisce l'item del carrello che corrisponde al prodotto che voglio aggiungere.
-  //Se lo trovo allora chiamo il metodo increaseQuantity dell'item stesso (metodo della classe CartItem di cui item è un'istanza). Questo metodo mi permette di aumentare la quantità di quel prodotto (valore salvato della proprietà quantity di CartItem).
-  //3. Se non c'è corrispondenza tra il ristorante del prodotto e quello del carrello, per il momento non faccio nulla.
-
-  addItem(product) {
-    if (this.isEmpty()) {
-      this._restaurant = product.restaurant_id;
-      const newItem = new CartItem(product);
-      this._items.push(newItem);
-      return true;
-    } else if (this._restaurant.id === product.restaurant_id) {
-      const item = this.findItem(product);
-      if (item) {
-        item.increaseQuantity();
-      } else {
-        const newItem = new CartItem(product);
-        this._items.push(newItem);
-      }
-      return true;
-    } else {
-      return false;
+    get amount() {
+        return this._items.reduce((sum, item) => sum + item.totalPrice, 0);
     }
-  }
 
-  //METODO REMOVEITEM()
-  //Metodo della classe Cart che mi permette di:
-  //1. Chiamare il metodo decreaseQuantity() di item che riduce la quantità del prodotto nel carrello (diminuisce la quantity salvata nella proprietà quantity della classe CartItem di cui item è un'istanza)
-  //2. Rimuovere il prodotto dal carrello se la quantity è 0 tramite uno splice fatto su items
-  removeItem(item) {
-    item.decreaseQuantity();
-    if (item.quantity === 0) {
-      const indexItem = this._items.indexOf(item);
-      this._items.splice(indexItem, 1);
+    get totalQuantity() {
+        return this._items.reduce((sum, item) => item.quantity, 0);
     }
-  }
 
-  //METODO CLEAR()
-  //Metodo della classe Cart che mi permette di svuotare il carrello ovvero di riportare le proprietà di cart ai loro valori di default (come definiti nel costruttore all'inizio)
-  clear() {
-    this._restaurant = null;
-    this._items = [];
-  }
+    get items() {
+        return this._items;
+    }
 
-  //METODO LOADFROMSTORAGE
-  //Questo metodo mi permette di riprendere il carrello salvato nel LocalStorage in modo che in seguito al ricarimento della pagina, il carrello
-  //sia ancora presente. Prende come argomento il carrello salvato nel LocalStorage.
-  //Il cart che si trova nel localStorage però è sottoforma di file Json quindi ho tutti i dati ma non riconosce che si tratti di un oggetto della classe Cart e a sua volta che items sono oggetti della classe CartItem quindi "perderei" tutti i metodi e le proprietà di queste classi.
-  //Vado quindi a ricostruire queste istanze manualmente:
-  // Assegno alla proprietà restaurant di cart, il valore di restaurant che ho nel LocalStorage
-  //Faccio lo stesso con la proprietà items pushandogli dentro delle nuove istanze di CartItem ed al costruttore gli passo i valori che ho nel LocalStorage
-  loadFromStorage(cartStorage) {
-    this._restaurant = cartStorage._restaurant;
-    cartStorage._items.forEach(item => {
-      this._items.push(new CartItem(item._product, item._quantity))});
-  }
+    get restaurant() {
+        return this._restaurant;
+    }
+
+    isEmpty() {
+        return this._items.length === 0;
+    }
+
+    findItem(product) {
+        if (this._restaurant && this._restaurant.id === product.restaurant_id) {
+            return this._items.find((item) => item.product.id === product.id);
+        } else {
+            return null;
+        }
+    }
+
+    addItem(product) {
+        if (this.isEmpty()) {
+            this._restaurant = product.restaurant_id;
+            const newItem = new CartItem(product);
+            this._items.push(newItem);
+            return true;
+        } else if (this._restaurant.id === product.restaurant_id) {
+            const item = this.findItem(product);
+            if (item) {
+                item.increaseQuantity();
+            } else {
+                const newItem = new CartItem(product);
+                this._items.push(newItem);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    removeItem(product) {
+        const item = this.findItem(product);
+        if (item) {
+            item.decreaseQuantity();
+            if (item.quantity === 0) {
+                const indexItem = this._items.indexOf(item);
+                this._items.splice(indexItem, 1);
+            }
+        }
+    }
+
+    clear() {
+        this._restaurant = null;
+        this._items = [];
+    }
+
+
+    loadFromStorage(cartStorage) {
+        this._restaurant = cartStorage._restaurant;
+        cartStorage._items.forEach((item) => {
+            this._items.push(new CartItem(item._product, item._quantity));
+        });
+    }
 }
--- resources/js/class/CartItem.js
+++ resources/js/class/CartItem.js
@@ -4,27 +4,22 @@ export default class CartItem {
       this._quantity = _quantity;
     }
 
-    //Getter della classe CartItem che restituisce la proprietà privata quantity ovvero la quantità di quel prodotto nel carrello
     get quantity() {
       return this._quantity;
     }
 
-    //Getter della classe CartItem che restituisce una sorta di proprietà custome dove salvo il prezzo totale di quel prodotto in base alla quantitò che abbiamo nel carrello
     get totalPrice() {
       return this._product.price * this._quantity;
     }
 
-    //Getter della classe CartItem che restituisce la proprietà privata product ovvero il prodotto stesso che ho nel carrello
     get product() {
       return this._product;
     }
 
-    //Metodo della classe CartItem che permette di aumentare la quantità del prodotto
     increaseQuantity() {
       this._quantity++;
     }
 
-    //Metodo della classe CartItem che permette di diminuire la quantià del prodotto solo se questa è maggiore di 0
     decreaseQuantity() {
       if (this._quantity > 0) {
         this._quantity--;

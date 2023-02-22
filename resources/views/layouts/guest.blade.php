30ce84839c8ce88465202407869a70d7a2869bc3
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-22 14:29:14 +0100

    add title and favicon in head

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>DeliveBoo</title>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href=" {{ Vite::asset('resources/img/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href=" {{ Vite::asset('resources/img/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href=" {{ Vite::asset('resources/img/favicon-16x16.png') }}">
+    <link rel="manifest" href="/site.webmanifest">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">

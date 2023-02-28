a70040c4905e10b98131566fc1089b1589b93c34
Stanghi <stangherlin.matti@gmail.com>
2023-02-28 17:13:49 +0100

    edit email

--- resources/views/email/new-order-customer.blade.php
+++ resources/views/email/new-order-customer.blade.php
@@ -1,28 +1,33 @@
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif
+    }
+</style>
 
-<p style="text-trasform: capitalize;">Ciao {{$order->name}},</p>
-<p>Grazie per aver ordinato su Deliveboo! Presto riceverai il tuo ordine da <span style="text-trasform: capitalize;">{{$order->restaurant->name}}</span></p>
+<p style="text-trasform: capitalize;">Ciao {{ $order->name }},</p>
+<p>Grazie per aver ordinato su Deliveboo! Presto riceverai il tuo ordine da <span
+        style="text-trasform: capitalize;">{{ $order->restaurant->name }}.</span></p>
 
 <h4 style="margin-top: 30px;">Riepilogo ordine:</h4>
 <ul>
     @foreach ($order->products as $product)
-        <li>{{$product->quantity}}{{$product->name}}</li>
+        <li>{{ $product->quantity }}{{ $product->name }}</li>
     @endforeach
 </ul>
-<h5>Totale: &euro;{{number_format($order->amount, 2, ',', ' ')}}</h5>
+<h5>Totale: &euro;{{ number_format($order->amount, 2, ',', ' ') }}</h5>
 
 <h5 style="margin-top: 30px;">Indirizzo di consegna inserito</h5>
-<p>{{$order->address}}</p>
+<p>{{ $order->address }}</p>
 
 @if ($order->note)
     <h5 style="margin-top: 20px;">Note lasciate per il ristorante</h5>
-    <p>{{$order->note}}</p>
+    <p>{{ $order->note }}</p>
 @endif
 
 
 <h5 style="margin-top: 20px;">Per qualsiasi necessità puoi contattare il ristorante:</h5>
-<p>{{$order->restaurant->telephone}}</p>
+<p>{{ $order->restaurant->telephone }}</p>
 
 <div class="logo" style="text-align: center; margin-top: 30px;">
     <img style="width: 100px;" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Logo">
 </div>
-
--- resources/views/email/new-order-restaurant.blade.php
+++ resources/views/email/new-order-restaurant.blade.php
@@ -1,12 +1,18 @@
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif
+    }
+</style>
 
 <h3>Hai ricevuto un nuovo ordine!</h3>
-<p style="text-trasform: capitalize; margin-top: 20px;">Cliente: {{$order->name}} {{$order->surname}}</p>
-<p>L'ordine deve essere consegnato presso {{$order->address}}</p>
+<p style="text-trasform: capitalize; margin-top: 20px;">Cliente: {{ $order->name }} {{ $order->surname }}.</p>
+<p>L'ordine deve essere consegnato presso {{ $order->address }}.</p>
 @if ($order->note)
-    <p style="margin-top: 20px;">Note dal cliente: {{$order->note}}</p>
+    <p style="margin-top: 20px;">Note dal cliente: {{ $order->note }}</p>
 @endif
 <div style="margin-top: 30px; text-align: center;">
-    <a style="text-align: center; text-decoration: none; font-weight:bold; padding: 10px; border-radius: 10px; background-color:#f15a25; color:#ffffff;"  href="http://127.0.0.1:8000/admin/orders/{{$order->id}}">Accedi alla tua area personale</a>
+    <a style="text-align: center; text-decoration: none; font-weight:bold; padding: 10px; border-radius: 10px; background-color:#f15a25; color:#ffffff;"
+        href="http://127.0.0.1:8000/admin/orders/{{ $order->id }}">Accedi alla tua area personale</a>
 </div>
 
 <div class="logo" style="text-align: center; margin-top: 40px;">

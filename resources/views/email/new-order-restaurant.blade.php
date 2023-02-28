23b945db399b5076123d12e57768363fcc810fb8
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-28 15:54:40 +0100

    add some style and content to emails send after order

--- resources/views/email/new-order-restaurant.blade.php
+++ resources/views/email/new-order-restaurant.blade.php
@@ -1,9 +1,14 @@
 
-<h2>Hai ricevuto un nuovo ordine</h2>
+<h3>Hai ricevuto un nuovo ordine!</h3>
+<p style="text-trasform: capitalize; margin-top: 20px;">Cliente: {{$order->name}} {{$order->surname}}</p>
 <p>L'ordine deve essere consegnato presso {{$order->address}}</p>
 @if ($order->note)
-    <p>Note dal cliente: {{$order->note}}</p>
+    <p style="margin-top: 20px;">Note dal cliente: {{$order->note}}</p>
 @endif
+<div style="margin-top: 30px; text-align: center;">
+    <a style="text-align: center; text-decoration: none; font-weight:bold; padding: 10px; border-radius: 10px; background-color:#f15a25; color:#ffffff;"  href="http://127.0.0.1:8000/admin/orders/{{$order->id}}">Accedi alla tua area personale</a>
+</div>
 
-
-
+<div class="logo" style="text-align: center; margin-top: 40px;">
+    <img style="width: 100px;" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Logo">
+</div>

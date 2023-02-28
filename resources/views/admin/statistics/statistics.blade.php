24fa703bb18cac9c3f1b59d5383fdd105d4dac17
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-02-28 09:53:43 +0100

    add check correct year in route for statistics by year

--- resources/views/admin/statistics/statistics.blade.php
+++ resources/views/admin/statistics/statistics.blade.php
@@ -9,7 +9,7 @@
         <h2 class="my-5">Statistiche Ristorante
             @if (request()->segment(2) == 'statistics' && request()->segment(3) == '')
                 <span> - Ultimi 6 mesi</span>
-            @elseif(request()->segment(4) == 'year')
+            @elseif(request()->segment(4) == 'year' && is_numeric(request()->segment(5)))
                 <span> - Anno {{ request()->segment(5) }} </span>
             @endif
 
@@ -22,14 +22,18 @@
             <a class="btn {{ request()->segment(2) == 'statistics' && request()->segment(3) == '' ? 'btn-g-active' : 'btn-outline-dark' }} "
                 href="{{ route('admin.statistics') }}">Ultimi 6 mesi</a>
         </div>
-        <div class="chart-box d-flex w-100">
-            <div class="card p-3 me-5">
-                <canvas id="chartOrders"></canvas>
+
+            <div class="chart-box d-flex w-100">
+                <div class="card p-3 me-5">
+                    <canvas id="chartOrders"></canvas>
+                </div>
+                <div class="card p-3">
+                    <canvas id="chartSales"></canvas>
+                </div>
             </div>
-            <div class="card p-3">
-                <canvas id="chartSales"></canvas>
-            </div>
-        </div>
+        @if (empty($statistics_data['numb_orders_by_month']['months']))
+            <h4 class="mt-3 text-center">Nessuna statistica presente</h4>
+        @endif
     </div>
 @endsection
 
